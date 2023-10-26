057d4fb1817b12063d01ab0d4c87185061e83dfa
Sagni <sagnialemayehu69@gmail.com>
2023-10-26 23:07:17 +0300

    cleaning the main.js

--- src/main.jsx
+++ src/main.jsx
@@ -5,40 +5,15 @@ import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
-
-import LoginPage from './LoginpPage';
-import Chat from './Chat';
-import GitHubLogin from './githubLogin';
-import GitHubCallback from './GitHubCallback';
 import App from './App';
 import { GoogleOAuthProvider } from '@react-oauth/google';
-import GoogleLogin from './GoogleLogin';
+
 
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
-  },
-  {
-    path: "/login",
-    element: <LoginPage />,
-  },
-  {
-    path: "/chat",
-    element: <Chat />,
-  },
-  {
-    path: "/github_login",
-    element: <GitHubLogin />,
-  },
-  {
-    path: "github-callback",
-    element: <GitHubCallback />,
-  },
-  {
-    path: "google-login",
-    element: <GoogleLogin />,
   },
 ]);
 
