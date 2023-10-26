7b8ae66fa1e2abbc08d1679b0f1eca4e536b8b44
Sagni <sagnialemayehu69@gmail.com>
2023-10-26 23:02:04 +0300

    google authentication added

--- src/GoogleLogin.jsx
+++ src/GoogleLogin.jsx
@@ -1,12 +1,24 @@
 import React from 'react'
 import { useGoogleLogin } from '@react-oauth/google';
+import axios from 'axios';
 
 
 
 const GoogleLogin = () => {
 
+  const googleAuth = async(accessToken) => {
+    const sentData = {"accessToken": accessToken}
+    const response = await axios.post('http://localhost:8000/api/auth/google',sentData,{
+      headers: {
+         'Content-Type': 'application/json',
+         'Accept': 'application/json',
+      }
+   });
+   console.log("form google auth: ",response)
+  }
+
     const login = useGoogleLogin({
-        onSuccess: tokenResponse => console.log("access token:",tokenResponse.access_token),
+        onSuccess: tokenResponse => googleAuth(tokenResponse.access_token),
       });
 
   return (
