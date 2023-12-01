d7b3622f0a31b2dd847475e68e4e7b026e33941e
Sagni <sagnialemayehu69@gmail.com>
2023-12-02 01:50:40 +0300

    ui distortion due to fixed position fixed

--- src/components/Layout/Chat.jsx
+++ src/components/Layout/Chat.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Chat = () => {
   return (
-    <div className='flex justify-end h-screen w-[48%] border-r pl-2'>
+    <div className='flex justify-end h-screen w-[48%]  border-r'>
   
     </div>
   )
--- src/components/Layout/RightSideBar.jsx
+++ src/components/Layout/RightSideBar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const RightSideBar = () => {
   return (
-    <div className='flex justify-end h-screen w-[25%]  bg-secondry'>
+    <div className='flex fixed justify-end h-screen w-[25%] ml-[75%] bg-secondry'>
 
     </div>
   )
