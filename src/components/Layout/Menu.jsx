b257ea2f00a79f0e3ddd2c506b500acbfbba0d38
Sagni <sagnialemayehu69@gmail.com>
2023-12-02 00:23:06 +0300

    menu bar

--- src/components/Layout/Menu.jsx
+++ src/components/Layout/Menu.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
+import { BsChatFill } from "react-icons/bs";
+import { BiSolidHomeAlt2 } from "react-icons/bi";
+
+
 
 const Menu = () => {
   return (
-    <div className='flex justify-center h-screen w-[6%]  fixed border-r'>
-      About
+    <div className='h-screen w-[6%]  fixed border-r'>
+      <div className='flex flex-col	justify-center content-center items-center gap-[18px] mt-16'>
+        <div className='flex flex-col justify-center items-center'>
+            <BiSolidHomeAlt2 className = "color-primary text-3xl mb-1" />
+            <p className='color-secondary text-sm text-center'>Home</p>
+        </div>
+        <div className='flex flex-col justify-center items-center'>
+            <BsChatFill className = "color-primary text-2xl	mb-1" />
+            <p className='color-secondary text-sm'>DM</p>
+        </div>
+        <div className='flex flex-col justify-center items-center'>
+            <BsChatFill className = "color-primary text-2xl	mb-1" />
+            <p className='color-secondary text-sm'>DM</p>
+        </div>
+      </div>    
+      
     </div>
   )
 }
