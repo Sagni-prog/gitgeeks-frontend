8161ac41d1155b90aff1995b4ef69a9e96165eef
Sagni <sagnialemayehu69@gmail.com>
2023-12-02 01:37:43 +0300

    ui fx

--- src/components/Layout/Menu.jsx
+++ src/components/Layout/Menu.jsx
@@ -10,8 +10,8 @@ import { RiSettings5Fill } from "react-icons/ri";
 const Menu = () => {
   return (
     <div className='h-screen w-[6%]  fixed flex flex-col items-center justify-between border-r'>
-      <div className='flex flex-col	justify-center content-center items-center gap-[18px] mt-16'>
-        <div className='flex flex-col justify-center items-center border-active-r w-[100%] cursor-pointer'>
+      <div className='flex flex-col	justify-center content-center items-center gap-[18px] mt-16 w-[100%]'>
+        <div className='flex flex-col justify-center items-center border-active-r w-[100%]'>
             <BiSolidHomeAlt2 className = "color-primary text-3xl mb-1" />
             <p className='color-secondary text-xs text-center'>Home</p>
         </div>
@@ -40,7 +40,6 @@ const Menu = () => {
       <div className='bg-secondary mb-6  bottom-profile cursor-pointer'>
         <img src = "https://avatars.githubusercontent.com/u/98890510?s=400&u=5bb16356e20b68aea2928951d56cda9347d5c77c&v=4" className='rounded-full border' />
       </div>  
-      
     </div>
   )
 }
