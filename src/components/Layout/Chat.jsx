7822f833c263e39f033bef8500956c5f9970b504
Sagni <sagnialemayehu69@gmail.com>
2023-12-02 22:30:19 +0300

    text editor added

--- src/components/Layout/Chat.jsx
+++ src/components/Layout/Chat.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import TextEditor from '../Elements/TextEditor'
 
 const Chat = () => {
   return (
     <div className='flex justify-end h-screen w-[48%]  border-r'>
-  
+        <TextEditor />
     </div>
   )
 }
