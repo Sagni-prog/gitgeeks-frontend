2fb3d50f3c78e9f248ea0fcf3e7b091efa28700b
Sagni <sagnialemayehu69@gmail.com>
2023-12-02 22:14:30 +0300

    ui fix

--- src/components/Elements/Channels.jsx
+++ src/components/Elements/Channels.jsx
@@ -2,20 +2,24 @@ import React from 'react'
 
 const Channels = () => {
   return (
-    <div className='flex flex-col justify-start items-start content-center w-[85%] mt-6'>
-      <h1>CHANNELS</h1>
-      <div className='flex flex-col mt-4 ml-2 gap-3 color-secondary'>
-            <div className='flex gap-5'>
-                <div className='bg-secondary  profile cursor-pointer'>
+    <div className='flex flex-col justify-start items-start content-center w-[100%] mt-6 '>
+      <h1 className='pl-[6%]'>CHANNELS</h1>
+      <div className='flex flex-col mt-1 w-[100%] h-[100%] color-secondary'>
+
+            <div className='cursor-pointer channel'>
+              <div className='ml-2 flex gap-5 pl-[8%] py-2'>
+                <div className=' profile cursor-pointer'>
                     <img src = "https://avatars.githubusercontent.com/u/98890510?s=400&u=5bb16356e20b68aea2928951d56cda9347d5c77c&v=4" className='rounded-full border' />
                 </div> 
 
                 <div>
                   <p>Haylie Carter</p>
                 </div> 
+              </div>  
             </div>
 
-            <div className='flex gap-5'>
+            <div className='cursor-pointer channel'>
+              <div className='ml-2 flex gap-5 pl-[8%] py-2'>
                 <div className='bg-secondary profile cursor-pointer'>
                     <img src = "https://avatars.githubusercontent.com/u/98890510?s=400&u=5bb16356e20b68aea2928951d56cda9347d5c77c&v=4" className='rounded-full border' />
                 </div> 
@@ -23,9 +27,11 @@ const Channels = () => {
                 <div>
                   <p>Mike Van</p>
                 </div> 
+              </div>  
             </div>
 
-            <div className='flex gap-5'>
+            <div className='cursor-pointer channel'>
+              <div className='ml-2 flex gap-5 pl-[8%] py-2'>
                 <div className='bg-secondary profile cursor-pointer'>
                     <img src = "https://avatars.githubusercontent.com/u/98890510?s=400&u=5bb16356e20b68aea2928951d56cda9347d5c77c&v=4" className='rounded-full border' />
                 </div> 
@@ -33,9 +39,12 @@ const Channels = () => {
                 <div>
                   <p>Mike Van</p>
                 </div> 
+
+              </div>
             </div>
 
-            <div className='flex gap-5'>
+            <div className='cursor-pointer channel'>
+              <div className='ml-2 flex gap-5 pl-[8%] py-2'>
                 <div className='bg-secondary  profile cursor-pointer'>
                     <img src = "https://avatars.githubusercontent.com/u/98890510?s=400&u=5bb16356e20b68aea2928951d56cda9347d5c77c&v=4" className='rounded-full border' />
                 </div> 
@@ -43,8 +52,9 @@ const Channels = () => {
                 <div>
                   <p>Haylie Carter</p>
                 </div> 
+              </div>  
             </div>
-            
+
         </div>
     </div>
   )
