f4d83519df196747a464171c7bfe33a9404ef5c6
Sagni <sagnialemayehu69@gmail.com>
2023-12-08 14:37:07 +0300

    login done

--- src/components/Layout/auth/Login.jsx
+++ src/components/Layout/auth/Login.jsx
@@ -4,13 +4,17 @@ import Input from '../../Elements/InputFields/Input';
 import Label from '../../Elements/Labels/Label';
 import { FaGithub } from "react-icons/fa";
 import { FcGoogle } from "react-icons/fc";
-
+import { MdDeviceHub } from "react-icons/md";
 
 
 const Login = () => {
   return (
     <div className='flex justify-center items-center content-center h-screen'>
        <div className='w-[40%] h-[100%] flex flex-col justify-center content-center items-center gap-3'>
+          <div className='flex flex-col justify-center items-center text-4xl color-primary mb-4'>
+             <MdDeviceHub className='text-1xl color-primary mb-3' />
+               <h3>Join the Nerd Community</h3>
+          </div>
            <Button
                 buttStyle = {{
                   width: "100%",
@@ -67,6 +71,10 @@ const Login = () => {
                 paddingLeft: "20px",
               }} 
           />
+          <div className='line w-[100%]'></div>
+          <div>
+             <p className='color-primary font-medium'>Already have an account? <a  href = '#' className='login text-cyan-400'>Log in</a>.</p>
+          </div>
        </div>  
     </div>
   )
